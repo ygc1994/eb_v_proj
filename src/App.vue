44635df1b9923ae1841fed92011e91d40af1f6fc
ygc1994 <ygc1994@naver.com>
2024-11-28 23:38:26 +0900

    commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import {ref} from "vue";
+  const apiUrl = import.meta.env.VITE_API_URL || 'https://helloggggg.shop';
+  const menuCode = ref(0);
+  const menu = ref('');
+
+  const findMenu = () => {
+   fetch(`${apiUrl}/menus/${menuCode.value}`)
+       .then(response => response.json())
+        .then(result => menu.value = result);
+  }
+
+</script>
+
+<template>
+  <h1>Elastic Beanstalk Test</h1>
+  메뉴 코드 : <input type="number" v-model="menuCode">
+  <button @click="findMenu">조회하기</button>
+  <p>{{ menu }}</p>
+</template>
+
+<style scoped>
+
+</style>
